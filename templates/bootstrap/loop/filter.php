b333a3b3b7bc5ce6cfc1dc2e6fe9192e7684b2d6
Kiran Prajapati <officeofkiran@gmail.com>
2025-06-06 21:23:57 +0530

    Add nofollow attribute to event filter dropdown links - CHANGED

--- templates/bootstrap/loop/filter.php
+++ templates/bootstrap/loop/filter.php
@@ -16,7 +16,7 @@
  *
  * @see        https://docs.wpgeodirectory.com/article/346-customizing-templates/
  * @package    GeoDir_Event_Manager
- * @version    2.2.3
+ * @version    2.3.20
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $options = '';
-$button_label = esc_attr__("Upcoming","geodirevents");
+$button_label = __( "Upcoming", "geodirevents" );
 foreach ( $event_types as $value => $label ) {
 	$url = add_query_arg( array( 'etype' => $value ), $current_url );
 	$url = apply_filters( 'geodir_event_type_filter_url', $url, $value );
@@ -34,19 +34,17 @@ foreach ( $event_types as $value => $label ) {
 	if( $event_type == $value ){
 		$button_label = $label;
 	}
-	$options .= '<a href="'.esc_url( $url ).'" data-etype="' . esc_attr( $value ) . '" class="dropdown-item '.$active.'">'.esc_attr( $label ).'</a>';
+	$options .= '<a href="' . esc_url( $url ) . '" data-etype="' . esc_attr( $value ) . '" class="dropdown-item ' . esc_attr( $active ) . '" rel="nofollow">' . esc_attr( $label ) . '</a>'; 
 }
 ?>
-<div class="btn-group btn-group-sm geodir-sort-by geodir-loop-event-filter" role="group" aria-label="<?php esc_attr_e("Filter Events","geodirevents");?>">
+<div class="btn-group btn-group-sm geodir-event-filter geodir-loop-event-filter" role="group" aria-label="<?php esc_attr_e("Filter Events","geodirevents");?>">
 	<div class="btn-group btn-group-sm" role="group">
-		<button id="geodir-sort-by" type="button" class="btn btn-outline-primary rounded-right rounded-end" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			<?php echo $button_label;?> <i class="fas fa-sort"></i>
-		</button>
+		<button id="geodir-event-filter" type="button" class="btn btn-outline-primary rounded-right rounded-end" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo esc_html( $button_label );?> <i class="fas fa-sort"></i></button>
 		<div class="dropdown-menu dropdown-caret-0 scrollbars-ios overflow-auto p-0 m-0 my-3" aria-labelledby="gd-list-view-select-grid" style="max-height: 50vh">
-			<h6 class="dropdown-header"><?php esc_attr_e("Filter Events","geodirevents");?></h6>
-			<?php echo $options;?>
+			<h6 class="dropdown-header"><?php echo esc_html__( "Filter Events", "geodirevents" ); ?></h6>
+			<?php echo $options; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped?>
 			<div class="dropdown-divider"></div>
-			<a class="dropdown-item" href="<?php echo esc_url( remove_query_arg( 'etype' ) );?>"><?php esc_attr_e("Default","geodirectory");?></a>
+			<a class="dropdown-item" href="<?php echo esc_url( remove_query_arg( 'etype' ) );?>" rel="nofollow"><?php echo esc_html__( "Default", "geodirectory" ); ?></a>
 		</div>
 	</div>
 </div>
